2f3ea180cc638c32c8573a52c29b3359a78cdca5
Carmen Geelhaar <camina.apps@gmail.com>
2022-07-10 17:37:09 +0200

    store bookshelf sort order in datastore

--- app/src/main/java/com/caminaapps/bookworm/core/data/repository/UserPreferencesRepository.kt
+++ app/src/main/java/com/caminaapps/bookworm/core/data/repository/UserPreferencesRepository.kt
@@ -1,4 +1,9 @@
 package com.caminaapps.bookworm.core.data.repository
 
+import com.caminaapps.bookworm.core.model.BookshelfSortOrder
+import kotlinx.coroutines.flow.Flow
+
 interface UserPreferencesRepository {
+    suspend fun saveBookshelfSortOrder(sortOrder: BookshelfSortOrder)
+    val bookshelfSortOrder: Flow<BookshelfSortOrder>
 }
---
+++ app/src/main/java/com/caminaapps/bookworm/core/model/BookshelfSortOrder.kt
@@ -0,0 +1,11 @@
+package com.caminaapps.bookworm.core.model
+
+
+enum class BookshelfSortOrder {
+    DATE_ADDED_ASC,
+    DATE_ADDED_DESC,
+    TITLE_ASC,
+    TITLE_DESC,
+    AUTHOR_ASC,
+    AUTHOR_DESC
+}
