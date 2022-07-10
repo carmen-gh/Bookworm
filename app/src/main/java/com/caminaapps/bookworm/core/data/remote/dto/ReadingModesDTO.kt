d9202e212cffcc18b0ca059322b2629528cc8a5f
Carmen Geelhaar <camina.apps@gmail.com>
2022-07-10 14:18:39 +0200

    remove old package

--- app/src/main/java/com/caminaapps/bookworm/core/data/remote/dto/ReadingModesDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.caminaapps.bookworm.core.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ReadingModesDTO(
-    val image: Boolean? = null,
-    val text: Boolean? = null
-)
--- app/src/main/java/com/caminaapps/bookworm/core/data/remote/dto/SearchInfoDTO.kt
+++
@@ -1,8 +0,0 @@
-package com.caminaapps.bookworm.core.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SearchInfoDTO(
-    val textSnippet: String? = null
-)
