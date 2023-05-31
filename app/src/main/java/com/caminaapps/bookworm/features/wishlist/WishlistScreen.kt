f24f72d6d8630af03d455c6125b20ab50051cce3
Carmen Geelhaar <camina.apps@gmail.com>
2023-05-31 21:18:58 +0200

    update code to meet the new rules

--- app/src/main/java/com/caminaapps/bookworm/features/wishlist/WishlistScreen.kt
+++ app/src/main/java/com/caminaapps/bookworm/features/wishlist/WishlistScreen.kt
@@ -2,12 +2,11 @@ package com.caminaapps.bookworm.features.wishlist
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.caminaapps.bookworm.core.ui.component.TrackedScreen
 
 @Composable
 fun WishlistScreen(
-    viewModel: WishlistViewModel = hiltViewModel()
+//    viewModel: WishlistViewModel = hiltViewModel()
 ) {
     TrackedScreen(name = "Wishlist")
     Text("- Wishlist -")
