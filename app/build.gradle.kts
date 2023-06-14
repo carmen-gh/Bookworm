580d4537cf1af552373b04b017f920545a7be0d7
Carmen Geelhaar <camina.apps@gmail.com>
2023-06-14 16:33:58 +0200

    chore remove obsolete annotations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,15 +2,6 @@ import com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA
 import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-// Without these suppressions version catalog usage here and in other build
-// files is marked red by IntelliJ:
-// https://youtrack.jetbrains.com/issue/KTIJ-19369.
-@Suppress(
-    "DSL_SCOPE_VIOLATION",
-    "MISSING_DEPENDENCY_CLASS",
-    "UNRESOLVED_REFERENCE_WRONG_RECEIVER",
-    "FUNCTION_CALL_EXPECTED"
-)
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.detekt)
