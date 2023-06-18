252681c41962a5ec4a3ae9a2916f1c317b114e6c
Carmen Geelhaar <camina.apps@gmail.com>
2023-06-18 15:25:18 +0200

    fix(book search): layout and tint color

--- app/src/main/java/com/caminaapps/bookworm/features/bookshelf/presentation/components/BookListItem.kt
+++ app/src/main/java/com/caminaapps/bookworm/features/bookshelf/presentation/components/BookListItem.kt
@@ -1,6 +1,5 @@
 package com.caminaapps.bookworm.features.bookshelf.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,13 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -72,13 +71,13 @@ fun BookListItem(
             }
 
             if (isFinished) {
-                Image(
+                Icon(
                     modifier = Modifier
                         .align(Alignment.BottomEnd)
                         .padding(16.dp),
                     painter = painterResource(id = R.drawable.ic_baseline_task_24),
                     contentDescription = "circle done",
-                    colorFilter = ColorFilter.tint(MaterialTheme.colors.secondary)
+                    tint = MaterialTheme.colors.secondary
                 )
             }
         }
---
+++ app/src/main/java/com/caminaapps/bookworm/util/complex_forwarding_painter.kt
@@ -0,0 +1,68 @@
+package com.caminaapps.bookworm.util
+
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
+
+
+// https://coil-kt.github.io/coil/recipes/#transforming-painters
+// Transforming Painters¶
+// Both AsyncImage and AsyncImagePainter have placeholder/error/fallback arguments that accept
+// Painters. Painters are less flexible than using composables, but are faster as Coil doesn't need
+// to use subcomposition. That said, it may be necessary to inset, stretch, tint, or transform your
+// painter to get the desired UI.
+
+/**
+ * Create and return a new [Painter] that wraps [painter] with its [alpha], [colorFilter], or [onDraw] overwritten.
+ */
+fun forwardingPainter(
+    painter: Painter,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+    onDraw: DrawScope.(ForwardingDrawInfo) -> Unit = DefaultOnDraw,
+): Painter = ForwardingPainter(painter, alpha, colorFilter, onDraw)
+
+data class ForwardingDrawInfo(
+    val painter: Painter,
+    val alpha: Float,
+    val colorFilter: ColorFilter?,
+)
+
+private class ForwardingPainter(
+    private val painter: Painter,
+    private var alpha: Float,
+    private var colorFilter: ColorFilter?,
+    private val onDraw: DrawScope.(ForwardingDrawInfo) -> Unit,
+) : Painter() {
+
+    private var info = newInfo()
+
+    override val intrinsicSize get() = painter.intrinsicSize
+
+    override fun applyAlpha(alpha: Float): Boolean {
+        if (alpha != DefaultAlpha) {
+            this.alpha = alpha
+            this.info = newInfo()
+        }
+        return true
+    }
+
+    override fun applyColorFilter(colorFilter: ColorFilter?): Boolean {
+        if (colorFilter != null) {
+            this.colorFilter = colorFilter
+            this.info = newInfo()
+        }
+        return true
+    }
+
+    override fun DrawScope.onDraw() = onDraw(info)
+
+    private fun newInfo() = ForwardingDrawInfo(painter, alpha, colorFilter)
+}
+
+private val DefaultOnDraw: DrawScope.(ForwardingDrawInfo) -> Unit = { info ->
+    with(info.painter) {
+        draw(size, info.alpha, info.colorFilter)
+    }
+}
